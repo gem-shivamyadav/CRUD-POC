f6527759a8b9ce24c964a67427aaaaac90a71099
shivam-gem <shivam.yadav@geminisolutions.com>
2022-07-26 11:22:01 +0530

    Heroku

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,6 +1,6 @@
 const prodURI =
   "mongodb+srv://shivamyadav:shivam7007@cluster0.1xfqotc.mongodb.net/?retryWrites=true&w=majority";
 const devURI = "mongodb://127.0.0.1:27017/student";
-const dbURI = process.env.NODE_ENV !== "production" ? prodURI : devURI;
+const dbURI = process.env.NODE_ENV === "production" ? prodURI : devURI;
 
 export { dbURI };
