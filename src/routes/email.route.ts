c5f590b5640dd65f79d3d2b90e11fcb439f17725
shivam-gem <shivam.yadav@geminisolutions.com>
2022-07-28 19:44:43 +0530

    Heroku

--- src/routes/email.route.ts
+++ src/routes/email.route.ts
@@ -1,6 +1,5 @@
 import express from "express";
 import { sendEmailController } from "../controllers/email.controller";
-
 const router = express.Router();
 
 router.post("/api/send-email", sendEmailController);
